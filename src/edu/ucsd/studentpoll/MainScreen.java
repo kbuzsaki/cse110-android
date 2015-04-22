8775f8d77e6f3dda8924b7722e038c4a6973a0f0
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-04-22 02:09:40 -0700

    added simple basic ui

--- src/edu/ucsd/studentpoll/MainScreen.java
+++ src/edu/ucsd/studentpoll/MainScreen.java
@@ -3,22 +3,17 @@ package edu.ucsd.studentpoll;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
-public class MyActivity extends Activity {
+public class MainScreen extends Activity {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.main);
-
-        Context context = getApplicationContext();
-        String result = GoogleIntegration.getAccountNames(context);
-
-        TextView tv = (TextView) findViewById(R.id.text_view);
-        tv.setText(result);
     }
 }
