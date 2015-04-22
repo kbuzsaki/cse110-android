50986e687aba51f8d7516bcd5ce70fba8294617a
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-04-22 02:49:47 -0700

    added simple single choice poll mock

--- src/edu/ucsd/studentpoll/MainScreen.java
+++ src/edu/ucsd/studentpoll/MainScreen.java
@@ -2,10 +2,16 @@ package edu.ucsd.studentpoll;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainScreen extends Activity {
     /**
@@ -15,5 +21,13 @@ public class MainScreen extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void joinPoll(View view) {
+        Intent intent = new Intent(this, SingleChoicePoll.class);
+        ArrayList<String> options = new ArrayList<>();
+        options.addAll(Arrays.asList("Cheese", "Pepperoni", "Sausage", "Mushroom", "Onion"));
+        intent.putExtra("options", options);
+        startActivity(intent);
     }
 }
