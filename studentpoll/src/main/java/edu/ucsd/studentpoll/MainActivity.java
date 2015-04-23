a43a4daa77f2a2491fef9ab93e88db79380bb9c4
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-04-22 18:13:03 -0700

    added content cards to the home view

--- studentpoll/src/main/java/edu/ucsd/studentpoll/MainActivity.java
+++ studentpoll/src/main/java/edu/ucsd/studentpoll/MainActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,22 +19,24 @@ import java.util.Arrays;
 
 public class MainActivity extends Activity {
 
+    private static int randInt(int min, int max) {
+        return (int)Math.floor(Math.random() * (max - min)) + min;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-//        LinearLayout contentView = (LinearLayout) findViewById(R.id.contentView);
-//
-//        CardView cardView = new CardView(this);
-//
-//        EditText editText = new EditText(this);
-//        editText.setText("card Text");
-//
-//        cardView.addView(editText);
-//        cardView.att
-//
-//        contentView.addView(cardView);
+        LinearLayout contentView = (LinearLayout) findViewById(R.id.contentView);
+        for(int i = 0; i < 10; i++) {
+            LayoutInflater.from(this).inflate(R.layout.poll_history_card, contentView);
+            View cardView = contentView.getChildAt(i);
+            String timeText = randInt(2, 21) + " minutes ago";
+            String voteText = randInt(0, 7) + "/" + randInt(7, 10) + " votes";
+            ((TextView)cardView.findViewById(R.id.time)).setText(timeText);
+            ((TextView)cardView.findViewById(R.id.votes)).setText(voteText);
+        }
     }
 
     public void joinPoll(View view) {
