a9d6b42d428c537b0a0f70a229aa7d92df13eb65
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-04-24 02:07:03 -0700

    added ability to add and remove poll options on create screen

--- studentpoll/src/main/java/edu/ucsd/studentpoll/CreateChoicePoll.java
+++ studentpoll/src/main/java/edu/ucsd/studentpoll/CreateChoicePoll.java
@@ -3,9 +3,17 @@ package edu.ucsd.studentpoll;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class CreateChoicePoll extends Activity {
@@ -22,6 +30,48 @@ public class CreateChoicePoll extends Activity {
 
         ((CheckBox)findViewById(R.id.allow_multiple_checkbox)).setChecked(allowMultiple);
         ((CheckBox)findViewById(R.id.allow_custom_checkbox)).setChecked(allowCustom);
+
+        EditText optionField = (EditText) findViewById(R.id.optionField);
+        addOptionFieldEnterListener((LinearLayout)optionField.getParent());
+    }
+
+    public void addPollOption(View view) {
+        LinearLayout clickedOptionContainer = (LinearLayout)view.getParent();
+        Button addButton = (Button)clickedOptionContainer.findViewById(R.id.optionAdd);
+        addButton.setText("x");
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removePollOption(view);
+            }
+        });
+
+        LinearLayout optionsContainer = (LinearLayout)clickedOptionContainer.getParent();
+        View newPollOption = LayoutInflater.from(this).inflate(R.layout.create_choice_poll_option, null);
+        addOptionFieldEnterListener((LinearLayout)newPollOption);
+        optionsContainer.addView(newPollOption);
+    }
+
+    public void removePollOption(View view) {
+        LinearLayout clickedOptionContainer = (LinearLayout)view.getParent();
+        LinearLayout optionsContainer = (LinearLayout)clickedOptionContainer.getParent();
+        optionsContainer.removeView(clickedOptionContainer);
+    }
+
+    private void addOptionFieldEnterListener(LinearLayout optionFieldLayout) {
+        EditText optionField = (EditText) optionFieldLayout.findViewById(R.id.optionField);
+        final Button addButton = (Button) optionFieldLayout.findViewById(R.id.optionAdd);
+
+        optionField.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE) {
+                    addButton.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
