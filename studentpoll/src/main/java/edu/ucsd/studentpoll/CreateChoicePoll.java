aaaa6e0d1adf110cbc40868f759437569b4f721a
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-05-10 04:56:06 -0700

    Added persistence to create choice poll

--- studentpoll/src/main/java/edu/ucsd/studentpoll/CreateChoicePoll.java
+++ studentpoll/src/main/java/edu/ucsd/studentpoll/CreateChoicePoll.java
@@ -28,6 +28,11 @@ public class CreateChoicePoll extends Activity {
 
     private static final String TAG = "CreateChoicePoll";
 
+    private static final String SAVED_TITLE = "savedTitle";
+    private static final String SAVED_OPTIONS = "savedOptions";
+    private static final String SAVED_ALLOW_MULTIPLE = "savedAllowMultiple";
+    private static final String SAVED_ALLOW_CUSTOM = "savedAllowCustom";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +47,67 @@ public class CreateChoicePoll extends Activity {
         ((CheckBox)findViewById(R.id.allow_custom_checkbox)).setChecked(allowCustom);
 
         EditText optionField = (EditText) findViewById(R.id.optionField);
-        addOptionFieldEnterListener((LinearLayout)optionField.getParent());
+        addOptionFieldEnterListener((LinearLayout) optionField.getParent());
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        String title = getQuestionTitle();
+        outState.putString(SAVED_TITLE, title);
+
+        ArrayList<String> options = getQuestionOptions();
+        outState.putStringArrayList(SAVED_OPTIONS, options);
+
+        boolean allowMultiple = getAllowMultiple();
+        outState.putBoolean(SAVED_ALLOW_MULTIPLE, allowMultiple);
+
+        boolean allowCustom = getAllowCustom();
+        outState.putBoolean(SAVED_ALLOW_CUSTOM, allowCustom);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if(savedInstanceState == null) {
+            return;
+        }
+
+        String title = savedInstanceState.getString(SAVED_TITLE);
+        List<String> options = savedInstanceState.getStringArrayList(SAVED_OPTIONS);
+        boolean allowMultiple = savedInstanceState.getBoolean(SAVED_ALLOW_MULTIPLE);
+        boolean allowCustom = savedInstanceState.getBoolean(SAVED_ALLOW_CUSTOM);
+
+        EditText titleBox = (EditText) findViewById(R.id.titleBox);
+        titleBox.setText(title);
+
+        // if we have saved state, then clear the default stuff
+        LinearLayout optionsLayout = (LinearLayout) findViewById(R.id.optionsLayout);
+        optionsLayout.removeAllViews();
+
+        for(int i = 0; i < options.size(); i++) {
+            String option = options.get(i);
+            View optionEntry = LayoutInflater.from(this).inflate(R.layout.create_choice_poll_option, null);
+            EditText optionText = (EditText) optionEntry.findViewById(R.id.optionField);
+            optionText.setText(option);
+            // set all but the last button to have an x for closing
+            if(i != options.size() - 1) {
+                Button addButton = (Button)optionEntry.findViewById(R.id.optionAdd);
+                addButton.setText("x");
+                addButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        removePollOption(view);
+                    }
+                });
+            }
+
+            addOptionFieldEnterListener((LinearLayout) optionEntry);
+            optionsLayout.addView(optionEntry);
+        }
+
+        ((CheckBox)findViewById(R.id.allow_multiple_checkbox)).setChecked(allowMultiple);
+        ((CheckBox)findViewById(R.id.allow_custom_checkbox)).setChecked(allowCustom);
     }
 
     public void addPollOption(View view) {
@@ -124,6 +189,34 @@ public class CreateChoicePoll extends Activity {
                 return false;
             }
         });
+    }
+
+    private String getQuestionTitle() {
+        EditText titleBox = (EditText) findViewById(R.id.titleBox);
+        return titleBox.getText().toString();
+    }
+
+    private ArrayList<String> getQuestionOptions() {
+        ArrayList<String> options = new ArrayList<>();
+
+        LinearLayout optionsLayout = (LinearLayout) findViewById(R.id.optionsLayout);
+        for(int i = 0; i < optionsLayout.getChildCount(); i++) {
+            View optionEntry = optionsLayout.getChildAt(i);
+            EditText optionBox = (EditText) optionEntry.findViewById(R.id.optionField);
+            options.add(optionBox.getText().toString());
+        }
+
+        return options;
+    }
+
+    private boolean getAllowMultiple() {
+        CheckBox allowMultipleCheckbox = (CheckBox) findViewById(R.id.allow_multiple_checkbox);
+        return allowMultipleCheckbox.isChecked();
+    }
+
+    private boolean getAllowCustom() {
+        CheckBox allowCustomCheckbox = (CheckBox) findViewById(R.id.allow_custom_checkbox);
+        return allowCustomCheckbox.isChecked();
     }
 
     @Override
